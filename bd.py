04cbcf8ff34aa74a09842a428b38cae87332b97a
IAAI2013 <isfahan.ifthikar@outlook.com>
2025-06-12 21:20:15 +0800

    Add files via upload

---
+++ bd.py
@@ -0,0 +1,11 @@
+birthday1 = "1981-05-11"
+birthday2 = "1986-07-30"
+birthday3 = "2013-08-14"
+birthday4 = "2022-03-11"
+birthday5 = "2025-08-20"
+
+print("My father's birthday is", birthday1)
+print("My mother's birthday is", birthday2)
+print("My birthday is", birthday3)
+print("My sister's birthday is", birthday4)
+print("My upcoming brother's birthday is", birthday5)
